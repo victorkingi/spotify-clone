aa1da2f6f7446dc38e122d84ad55103ad1854402
victorkingi <65971651+victorkingi@users.noreply.github.com>
2020-09-20 16:19:12 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
 import './App.css';
+import Login from "./Login";
+import {getTokenFromUrl} from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./Player";
+import { useDataLayerValue } from "./DataLayer";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+    const [{ token }, dispatch] = useDataLayerValue();
+
+    useEffect(() => {
+        const hash = getTokenFromUrl();
+        window.location.hash = "";
+        const _token = hash.access_token;
+
+        if (_token) {
+            dispatch({
+                type: 'SET_TOKEN',
+                token: _token
+            })
+            
+            spotify.setAccessToken(_token);
+
+            spotify.getMe().then(user => {
+
+                dispatch({
+                    type: 'SET_USER',
+                    user: user
+                })
+            })
+
+            spotify.getUserPlaylists().then((playlists) => {
+                dispatch({
+                    type: 'SET_PLAYLISTS',
+                    playlists: playlists
+                })
+            });
+
+            /**TODO For Developer: replace 'YOUR_DISCOVER_WEEKLY_KEY' with the actual key found on
+             * https://open.spotify.com/playlist/DISCOVER_WEEKLY_KEY when you open your discoverWeekly
+             * playlist This is how the key looks like: '68w3rQZEGHcF4yK1GZKF3g'
+             */
+
+            spotify.getPlaylist('37i9dQZEVXcF4yK1GZKF3g').then(response => {
+                dispatch({
+                    type: 'SET_DISCOVER_WEEKLY',
+                    discover_weekly: response
+
+                })
+            })
+        }
+
+    }, [token, dispatch]);
+
+    console.log(token)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        {
+            token ? (
+                <Player spotify={spotify} />
+            ) : (
+                <Login />
+            )
+        }
     </div>
   );
 }
